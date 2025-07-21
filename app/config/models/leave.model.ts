26d23418c9a292fb56508e22eccbe811a8b90a35
khainghieply <khainghieply@gmail.com>
2025-07-21 08:08:53 +0700

    adding more features

--- app/config/models/leave.model.ts
+++ app/config/models/leave.model.ts
@@ -1,6 +1,16 @@
 export interface Leave {
-  leaveType: number;
+  leaveId: string;
+  workerId: string;
+  leaveType: string;
   startDate: string;
   endDate: string;
+  totalDays: number;
   reason: string;
+  requestDate: string;
+  approvalStatus: string;
+  approvedBy: string | null;
+  approvalDate: string | null;
+  note: string | null;
+  approvedByNavigation?: any | null;
+  worker?: any | null;
 }
